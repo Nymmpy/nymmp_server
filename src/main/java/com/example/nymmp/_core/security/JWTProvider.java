73a2638c10318211309536ede211aaec98580129
Cheoroo <195822@jnu.ac.kr>
2024-07-08 04:04:52 +0900

    feat: Add JWTProvider for creating and verifying JWT tokens
    - 사용자 정보를 포함한 JWT 토큰 생성 메서드 구현.
    - JWT 토큰을 검증하고 디코딩하는 메서드 추가.
    - 토큰 서명을 위해 HMAC512 알고리즘과 비밀 키 사용.

---
+++ src/main/java/com/example/nymmp/_core/security/JWTProvider.java
@@ -0,0 +1,32 @@
+package com.example.nymmp._core.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.nymmp.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTProvider {
+    public static final Long EXP = 1000L * 60 * 60 * 48; // 48시간
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER = "Authorization";
+    private static final String SECRET = "MySecretKey";
+
+    public static String create(User user) {
+        return JWT.create()
+                .withSubject(user.getEmail())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXP))
+                .withClaim("id", user.getUserId())
+                .withClaim("group", user.getGroup().getGroupName())
+                .sign(Algorithm.HMAC512(SECRET));
+    }
+
+    public static DecodedJWT verify(String jwt) throws SignatureVerificationException, TokenExpiredException {
+        return JWT.require(Algorithm.HMAC512(SECRET)).build().verify(jwt.replace(TOKEN_PREFIX, ""));
+    }
+}
