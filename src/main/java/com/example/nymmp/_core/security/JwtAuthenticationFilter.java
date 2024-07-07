e62e55a316dac957ce345d02bfbacf4bf80b240b
Cheoroo <195822@jnu.ac.kr>
2024-07-08 04:04:39 +0900

    feat: Add JwtAuthenticationFilter for JWT-based authentication
    - BasicAuthenticationFilter를 확장하여 JWT 인증 구현.
    - 요청 헤더에서 JWT 토큰 검증.
    - 데이터베이스에서 사용자 정보를 로드하고 인증 컨텍스트 설정.
    - 토큰 검증 및 만료 예외 처리.

---
+++ src/main/java/com/example/nymmp/_core/security/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.example.nymmp._core.security;
+
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.nymmp.model.User;
+import com.example.nymmp.repository.UserJPARepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+    private final UserJPARepository userJPARepository;
+
+    @Autowired
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, UserJPARepository userJPARepository) {
+        super(authenticationManager);
+        this.userJPARepository = userJPARepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String prefixJwt = request.getHeader("Authorization");
+        if (prefixJwt == null) {
+            chain.doFilter(request, response);
+        } else {
+            String jwt = prefixJwt.replace("Bearer ", "");
+
+            try {
+                log.debug("디버그 : 토큰 있음");
+                DecodedJWT decodedJWT = JWTProvider.verify(jwt);
+                Long userId = decodedJWT.getClaim("id").asLong();
+                User user = userJPARepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found", new Exception()));
+                CustomUserDetails myUserDetails = new CustomUserDetails(user);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(myUserDetails, myUserDetails.getPassword(), myUserDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.debug("디버그 : 인증 객체 만들어짐");
+            } catch (SignatureVerificationException e) {
+                log.error("토큰 검증 실패", e);
+            } catch (TokenExpiredException e) {
+                log.error("토큰 만료됨", e);
+            } finally {
+                chain.doFilter(request, response);
+            }
+        }
+    }
+}
