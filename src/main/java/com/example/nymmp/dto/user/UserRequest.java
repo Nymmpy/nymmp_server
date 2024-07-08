9296f617226e5160a9f9097b20e12abdf7aa6d97
Cheoroo <195822@jnu.ac.kr>
2024-07-08 15:46:08 +0900

    feat: add DTO about User
    UserRequest추가 (JoinDTO, LoginDTO, EmailCheckDTO, UpdatePasswordDTO)
    UserResponse추가 (FindById, LoginResponse)

--- src/main/java/com/example/nymmp/dto/user/UserRequest.java
+++ src/main/java/com/example/nymmp/dto/user/UserRequest.java
@@ -1,4 +1,70 @@
 package com.example.nymmp.dto.user;
 
+import com.example.nymmp.model.Group;
+import com.example.nymmp.model.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class UserRequest {
+    @Getter
+    @Setter
+    public static class JoinDTO {
+        @NotBlank(message = "email is required.")
+        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "Please enter a valid email address")
+        private String email;
+
+        @NotBlank(message = "password is required.")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!~`<>,./?;:'\"\\[\\]{}\\\\()|_-])\\S*$", message = "It must contain letters, numbers, and special characters, and cannot contain spaces")
+        private String password;
+
+        @NotBlank(message = "name is required.")
+        @Size(min = 2, max = 20, message = "Name must be between 2 and 20 characters")
+        private String name;
+
+        private String groupName;
+
+        public User toEntity(String encodedPassword, Group group) {
+            return User.builder()
+                    .email(email)
+                    .password(encodedPassword)
+                    .username(name)
+                    .group(group)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class LoginDTO {
+        @NotBlank(message = "email is required.")
+        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "Please enter a valid email address.")
+        private String email;
+
+        @NotBlank(message = "password is required.")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!~`<>,./?;:'\"\\[\\]{}\\\\()|_-])\\S*$", message = "It must contain letters, numbers, and special characters, and cannot contain spaces.")
+        private String password;
+    }
+
+    @Getter
+    @Setter
+    public static class EmailCheckDTO {
+        @NotBlank(message = "email is required.")
+        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "Please enter a valid email address")
+        private String email;
+    }
+
+    @Getter
+    @Setter
+    public static class UpdatePasswordDTO {
+        @NotBlank(message = "password is required.")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!~`<>,./?;:'\"\\[\\]{}\\\\()|_-])\\S*$", message = "It must contain letters, numbers, and special characters, and cannot contain spaces")
+        private String password;
+    }
 }
--- src/main/java/com/example/nymmp/dto/user/UserResponse.java
+++ src/main/java/com/example/nymmp/dto/user/UserResponse.java
@@ -1,4 +1,33 @@
 package com.example.nymmp.dto.user;
 
+import com.example.nymmp.model.User;
+import lombok.Getter;
+import lombok.Setter;
+
 public class UserResponse {
+    @Getter
+    @Setter
+    public static class FindById {
+        private long userId;
+        private String username;
+        private String email;
+
+        public FindById(User user) {
+            this.userId = user.getUserId();
+            this.username = user.getUsername();
+            this.email = user.getEmail();
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class LoginResponse {
+        private String jwtToken;
+        private String redirectUrl;
+
+        public LoginResponse(String jwtToken, String redirectUrl) {
+            this.jwtToken = jwtToken;
+            this.redirectUrl = redirectUrl;
+        }
+    }
 }
