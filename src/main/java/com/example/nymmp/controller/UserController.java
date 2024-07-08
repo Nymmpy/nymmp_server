36a7832a90dc9966a94187ccc7e4c7ac496620f5
Cheoroo <195822@jnu.ac.kr>
2024-07-08 15:48:08 +0900

    feat: userService and UserController
    회원가입을 위한 서비스 추가(join, login, and email validation.)
    로그인 컨트롤러 추가

---
+++ src/main/java/com/example/nymmp/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.nymmp.controller;
+
+import com.example.nymmp._core.security.JWTProvider;
+import com.example.nymmp._core.utils.ApiUtils;
+import com.example.nymmp.dto.user.UserRequest;
+import com.example.nymmp.dto.user.UserResponse;
+import com.example.nymmp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/check")
+    public ResponseEntity<?> checkMember(@RequestBody @Valid UserRequest.EmailCheckDTO emailCheckDTO, Errors errors) {
+        userService.checkSameEmail(emailCheckDTO.getEmail());
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<?> joinUser(@RequestBody @Valid UserRequest.JoinDTO requestDTO, Errors errors) {
+        userService.join(requestDTO);
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody @Valid UserRequest.LoginDTO requestDTO, Errors errors) {
+        UserResponse.LoginResponse response = userService.login(requestDTO);
+        return ResponseEntity.ok().header(JWTProvider.HEADER, response.getJwtToken()).body(ApiUtils.success(null));
+    }
+}
--- src/main/java/com/example/nymmp/service/UserService.java
+++ src/main/java/com/example/nymmp/service/UserService.java
@@ -1,18 +1,64 @@
 package com.example.nymmp.service;
 
+import com.example.nymmp._core.exception.Exception401;
+import com.example.nymmp._core.exception.Exception403;
+import com.example.nymmp._core.exception.Exception404;
+import com.example.nymmp._core.exception.Exception500;
+import com.example.nymmp._core.security.JWTProvider;
+import com.example.nymmp.dto.user.UserRequest;
+import com.example.nymmp.dto.user.UserResponse;
+import com.example.nymmp.model.Group;
 import com.example.nymmp.model.User;
+import com.example.nymmp.repository.GroupRepository;
 import com.example.nymmp.repository.UserJPARepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Optional;
 
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Service
 public class UserService {
-    @Autowired
-    private UserJPARepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserJPARepository userJPARepository;
+    private final GroupRepository groupRepository;
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    @Transactional
+    public void join(UserRequest.JoinDTO requestDTO) {
+        if (userJPARepository.findByEmail(requestDTO.getEmail()).isPresent()) {
+            throw new Exception403("이미 존재하는 이메일입니다.");
+        }
+        String encodedPassword = passwordEncoder.encode(requestDTO.getPassword());
+        Group group = groupRepository.findByGroupName(requestDTO.getGroupName())
+                .orElseThrow(() -> new Exception404("그룹을 찾을 수 없습니다: " + requestDTO.getGroupName()));
+        User user = requestDTO.toEntity(encodedPassword, group);
+        try {
+            userJPARepository.save(user);
+        } catch (Exception e) {
+            throw new Exception500("알 수 없는 서버 오류가 발생했습니다.");
+        }
+    }
+
+    public UserResponse.LoginResponse login(UserRequest.LoginDTO requestDTO) {
+        User user = userJPARepository.findByEmail(requestDTO.getEmail()).orElseThrow(
+                () -> new Exception404("이메일을 찾을 수 없습니다: " + requestDTO.getEmail())
+        );
+        if (!passwordEncoder.matches(requestDTO.getPassword(), user.getPassword())) {
+            throw new Exception401("비밀번호가 일치하지 않습니다.");
+        }
+        String jwt = JWTProvider.create(user);
+        String redirectUrl = "/home";
+
+        return new UserResponse.LoginResponse(jwt, redirectUrl);
+    }
+
+    public void checkSameEmail(String email) {
+        Optional<User> userOptional = userJPARepository.findByEmail(email);
+        if (userOptional.isPresent()) {
+            throw new Exception403("이미 존재하는 이메일입니다: " + email);
+        }
     }
 }
